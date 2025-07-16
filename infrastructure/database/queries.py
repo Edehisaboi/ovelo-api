2ab9c6e6cb54eb486c63064a071e054f4f28fdb9
Israel Edeh <israeledeh11@gmail.com>
2025-07-16 08:12:04 +0100

    Refactor vector_search: update function signature, integrate MongoDBAtlasHybridSearchRetriever, and simplify vector store handling logic.

--- infrastructure/database/queries.py
+++ infrastructure/database/queries.py
@@ -1,4 +1,7 @@
-from typing import List, Dict
+from typing import List, Dict, Union, Tuple, Optional
+
+from langchain_core.documents import Document
+from langchain_mongodb.retrievers import MongoDBAtlasHybridSearchRetriever
 
 from application.core.config import settings
 from application.core.logging import get_logger
@@ -10,9 +13,9 @@ logger = get_logger(__name__)
 
 
 async def search_by_title(
-    manager: MongoCollectionsManager,
-    query: str,
-    limit: int = settings.MAX_RESULTS_PER_PAGE
+    manager:  MongoCollectionsManager,
+    query:    str,
+    limit:    int = settings.MAX_RESULTS_PER_PAGE
 ) -> tuple[List[MovieDetails], List[TVDetails]]:
     """
     Text search for both movies and TV shows using $text and $unionWith.
@@ -97,49 +100,32 @@ async def search_by_title(
         raise
 
 
-#
-# async def vector_search(
-#     wrapper:    CollectionWrapper,
-#     query:      str,
-#     limit:      int = settings.MAX_RESULTS_PER_PAGE,
-#     filter_criteria: Optional[Dict] = None
-# ) -> Union[List[Tuple[Document, float]], List[Document]]:
-#     """
-#     Perform a hybrid vector and text search using the collection's hybrid retriever.
-#
-#     Args:
-#         wrapper (CollectionWrapper): Collection wrapper with an initialized retriever.
-#         query (str): The search query.
-#         limit (int, optional): Max number of results to return.
-#         filter_criteria (dict, optional): MongoDB pre_filter for vector search (if supported).
-#
-#     Returns:
-#         Union[List[Tuple[Document, float]], List[Document]]:
-#             - List of (Document, score) tuples if using similarity_search_with_score.
-#             - List of Document if using retriever.invoke.
-#     """
-#     try:
-#         # TODO: Check if the collection has a hybrid retriever initialized, IT DOES NOT CHECK IF THE COLLECTION IS A VECTOR STORE
-#         retriever = getattr(wrapper, "retriever", None)
-#         if not retriever:
-#             raise ValueError(
-#                 "Hybrid search retriever not initialized for this collection."
-#             )
-#         vectorstore = retriever.vectorstore
-#
-#         if vectorstore:
-#             # Returns List[Tuple[Document, float]]
-#             documents = await vectorstore.asimilarity_search_with_score(
-#                 query=query,
-#                 k=limit,
-#                 pre_filter=filter_criteria
-#             )
-#         else:
-#             # Returns List[Document]
-#             documents = await retriever.ainvoke(query)
-#
-#         return documents
-#
-#     except Exception as e:
-#         logger.error(f"Error performing vector search: {e}")
-#         raise
+async def vector_search(
+    retriever:  MongoDBAtlasHybridSearchRetriever,
+    query:      str,
+    limit:      int = settings.MAX_RESULTS_PER_PAGE,
+    filter_criteria: Optional[Dict] = None
+) -> Union[List[Tuple[Document, float]], List[Document], None]:
+    try:
+        if not retriever:
+            raise ValueError(
+                "Hybrid search retriever not initialized for this collection."
+            )
+        vector_store = retriever.vectorstore
+
+        if vector_store:
+            # Returns List[Tuple[Document, float]]
+            documents = await vector_store.asimilarity_search_with_score(
+                query=query,
+                k=limit,
+                pre_filter=filter_criteria
+            )
+        else:
+            # Returns List[Document]
+            documents = await retriever.ainvoke(query)
+
+        return documents
+
+    except Exception as e:
+        logger.error(f"Error performing vector search: {e}")
+        raise
